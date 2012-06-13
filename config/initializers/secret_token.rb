a0d4a87d964ff8b8d20ad3c9d8ab3c1708677cfc
Derek Ethier <derek.ethier@gmail.com>
2012-06-12 22:00:08 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headquartr::Application.config.secret_token = '349c0b0461635362667295cd3e9377f5cbe6eb16730d6e88039e4e6ecde36cf779ee7fd39ec6440cf48a855b6eadb4377c22c643eb12590fe41cc80da1e93358'
